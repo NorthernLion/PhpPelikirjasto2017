a82de5d41f44ce2758499e90e720fe97d55fe30e
NorthernLion <joppetaja@gmail.com>
2017-03-22 11:20:43 +0200

    Hienosäätöä

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -18,6 +18,8 @@
 
       // Omat !
 
+      //Klubi
+
       public static function club_create(){
           View::make('club_create.html');
       }
@@ -29,6 +31,8 @@
       public static function club_show(){
           View::make('club_show');
       }
+
+      //Peli
 
       public static function game_create(){
           View::make('game_create.html');
@@ -46,11 +50,31 @@
           View::make('game_show.html');
       }
 
+      //Käyttäjä
+
       public static function player_create(){
           View::make('game_list.html');
       }
 
       public static function player_show(){
-          View::make('helloworld.html');
+          View::make('player_show.html');
+      }
+
+      //Strategia
+
+      public static function strategy_list(){
+          View::make('strategy_list.html');
+      }
+
+      public static function strategy_modify(){
+          View::make('strategy_modify.html');
+      }
+
+      public static function strategy_show(){
+          View::make('strategy_show.html');
+      }
+
+      public static function strategy_create(){
+          View::make('strategy_list.html');
       }
   }
--- config/routes.php
+++ config/routes.php
@@ -1,24 +1,81 @@
 <?php
 
-  $routes->get('/', function() {
-    HelloWorldController::index();
-  });
+      $routes->get('/', function() {
+        HelloWorldController::index();
+      });
 
-  $routes->get('/hiekkalaatikko', function() {
-    HelloWorldController::sandbox();
-  });
-  
-    $routes->get('/pelit', function() {
-    HelloWorldController::lista();
-  });
+      $routes->get('/hiekkalaatikko', function() {
+        HelloWorldController::sandbox();
+      });
 
-    $routes->get('/game', function() {
-        HelloWorldController::game_list();
-    });
-    $routes->get('/game/1', function() {
-        HelloWorldController::game_show();
-    });
+        $routes->get('/pelitvanha', function() {
+        HelloWorldController::lista();
+      });
+
+// Omat !
 
     $routes->get('/login', function() {
         HelloWorldController::login();
     });
+
+//Peli
+
+$routes->get('/game', function() {
+    HelloWorldController::game_list();
+});
+
+$routes->get('/game/1', function() {
+    HelloWorldController::game_show();
+});
+
+$routes->get('/game/create', function() {
+    HelloWorldController::game_create();
+});
+
+$routes->get('/game/modify/1', function() {
+    HelloWorldController::game_modify();
+});
+
+//Käyttäjä
+
+$routes->get('/user/1', function() {
+    HelloWorldController::player_show();
+});
+
+$routes->get('/user/create', function() {
+    HelloWorldController::player_create();
+});
+
+//Strategia
+
+$routes->get('/strategy', function() {
+    HelloWorldController::strategy_list();
+});
+
+$routes->get('/strategy/1', function() {
+    HelloWorldController::strategy_show();
+});
+
+$routes->get('/strategy/create', function() {
+    HelloWorldController::strategy_create();
+});
+
+$routes->get('/game/modify/1', function() {
+    HelloWorldController::strategy_modify();
+});
+
+//Klubi
+
+$routes->get('/club', function() {
+    HelloWorldController::club_list();
+});
+
+$routes->get('/strategy/1', function() {
+    HelloWorldController::club_show();
+});
+
+$routes->get('/strategy/create', function() {
+    HelloWorldController::club_create();
+});
+
+
